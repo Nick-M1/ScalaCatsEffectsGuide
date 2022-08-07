eb62f34f59a0b831efea01d2b4d4a64b19490a98
Nick-M1 <nicholasmalloura@gmail.com>
2022-08-07 20:20:01 +0100

    Version 1

--- src/main/scala/TESTING2.scala
+++ src/main/scala/TESTING2.scala
@@ -1,10 +1,16 @@
 import cats.effect.{IO, IOApp}
-import utils_useful.extensions.debug
-import concurrent.duration.DurationInt
 
-//
-//object TESTING2 extends IOApp.Simple {
-//
-//
-//
-//}
+import scala.concurrent.Future
+import scala.util.{Failure, Success}
+import concurrent.ExecutionContext.Implicits.global
+import utils_useful.extensions.debug
+import cats.effect.Async
+import cats.effect.implicits
+import cats.effect.IO.asyncForIO
+import cats.implicits._
+
+object TESTING2 extends IOApp.Simple {
+
+
+  override def run: IO[Unit] = ???
+}
